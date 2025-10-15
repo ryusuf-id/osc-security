d68663e629ffda4201467bb29d080bab845737be
ryusuf-id <mail.riswandha@gmail.com>
2025-10-15 08:03:04 +0700

    Create script-main.ps1

---
+++ script-main.ps1
@@ -0,0 +1,3 @@
+$ProgressPreference = 'SilentlyContinue'
+cd C:/elastic-agent/elastic-agent
+.\elastic-agent.exe install --url=https://172.31.1.244:8220 --enrollment-token=Z0x2clRINEJURjZ4WUI4VHBRdk46RXlHcFV5bldTZFM5T3BnYUZQVVZ3dw== --insecure
